02a986474608e64f161d8ead7534b537614ede35
Tiger Soldier <tigersoldi@gmail.com>
2011-11-21 17:49:26 +0900

    Make use of standard API addToStatusArea

--- extensions/ibus-indicator@example.com/extension.js
+++ extensions/ibus-indicator@example.com/extension.js
@@ -1,48 +1,23 @@
 /* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
 
 const Main = imports.ui.main;
-const Panel = imports.ui.panel;
-const PopupMenu = imports.ui.popupMenu;
 const Indicator = imports.ui.status.ibus.indicator;
 
 let indicator = null;
-let menus = null;
 
 function init() {
-}
-
-function main() {
-    // The gettext is fixed in gnome-shell 3.1.4 or later at least.
-    if (window._ == undefined) {
-        const Shell = imports.gi.Shell;
-        const Gettext = imports.gettext;
-        window.global = Shell.Global.get();
-        window._ = Gettext.gettext;
-        window.C_ = Gettext.pgettext;
-        window.ngettext = Gettext.ngettext;
-    }
-    Panel.STANDARD_TRAY_ICON_ORDER.push('ibus');
-    Panel.STANDARD_TRAY_ICON_SHELL_IMPLEMENTATION['ibus'] = Indicator.Indicator;
 }
 
 function enable() {
     if (!indicator) {
         indicator = new Indicator.Indicator();
+        Main.panel.addToStatusArea('ibus', indicator, 0);
     }
-    if (!menus) {
-        menus = new PopupMenu.PopupMenuManager(indicator);
-    }
-    menus.addMenu(indicator.menu);
-    Main.statusIconDispatcher.emit('status-icon-added', indicator.actor, 'ibus');
 }
 
 function disable() {
     if (indicator) {
-        if (menus) {
-            menus.removeMenu(indicator.menu);
-            menus = null;
-        }
-        Main.statusIconDispatcher.emit('status-icon-removed', indicator.actor);
+        indicator.destroy();
         indicator = null;
     }
 }
